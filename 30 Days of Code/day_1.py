c7797ba7236749fd43412b383fc5c6633172dd9e
alex-waiganjo <alexwaiganjo934@gmail.com>
2023-02-21 00:20:28 +0300

    day 1

---
+++ 30 Days of Code/day_1.py
@@ -0,0 +1,46 @@
+# Task
+'''
+Complete the code in the editor below. The variables i ,d and s are already declared and initialized for you. You must:
+
+1. Declare 3 variables: one of type int, one of type double, and one of type String.
+2. Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
+3. Use the  operator to perform the following operations:
+     a. Print the sum of i plus your int variable on a new line.
+     b. Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+     c. Concatenate s with the string you read as input and print the result on a new line.
+'''
+
+
+# Sample Input
+        #   a. 12
+        #   b. 4.0
+        #   c. is the best place to learn and practice coding!
+
+# Sample Output
+        #   a. 16
+        #   b. 8.0
+        #   c. HackerRank is the best place to learn and practice coding!    
+
+
+#Solution
+i = 12
+d = 4.0
+s = 'HackerRank '
+
+i2 = int(input()) # read int
+d2 = float(input()) # read double 
+s2 = input() # read string
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(i+i2)
+print(d2+d2)
+print(s+s2) 

e97d9e400a17c9c3f4d96cefdb082b2c1682f49a
alex-waiganjo <alexwaiganjo934@gmail.com>
2023-02-23 00:38:44 +0300

    SQL

--- SQL Track/Easy/WeatherStationObservation.sql
+++ SQL Track/Easy/WeatherStationObservation.sql
@@ -15,7 +15,6 @@ Where LAT_N is the northern latitude and LONG_W is the western longitude.
 '
 TASK 1
 Query a list of CITY and STATE from the STATION table.'
-
 'SOLUTION '
 SELECT CITY,STATE FROM STATION;
 
@@ -24,6 +23,5 @@ SELECT CITY,STATE FROM STATION;
 TASK 2
 Query a list of CITY names from STATION for cities that have an even ID number.
 Print the results in any order, but exclude duplicates from the answer.'
-
 'SOLUTION'
 SELECT  DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;

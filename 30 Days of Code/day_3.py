698b81fed5ec675b6143e44e37c3ad03912dafd3
alex-waiganjo <alexwaiganjo934@gmail.com>
2023-02-26 23:52:43 +0300

    day 3

---
+++ 30 Days of Code/day_3.py
@@ -0,0 +1,36 @@
+#TASK
+'''
+Given an integer ,n ,Perform the following conditional actions:
+   => If n is odd,Print 'Weird'
+   => If n is even and in the inclusive of 2 to 5, Print 'Not Weird'
+   => If n is even and in the inclusive of 6 to 20 ,Print  'Weird'
+   => If n is even and greater than 20 ,Print 'Not Weird'
+ '''
+
+
+ #SOLUTION
+if __name__ == '__main__':
+    N = int(input().strip())
+
+
+#  Method 1        ------- Good Readable Code -----
+if N%2 ==0:
+    if N in range(2,5):
+        print('Not Weird')
+    if N==20 or N in range(6,20):
+        print('Weird')
+    if N>20:
+        print('Not Weird')        
+else:
+     print('Weird')    
+
+
+#  Method 2       ------ Poorly Structured Code -------
+if N%2 == 1:
+    print('Weird')
+if N%2 == 0 and N in range(2,5):
+    print('Not Weird')
+if N%2 == 0 and N==20 or N in range(6,20):
+    print('Weird')
+if N%2 == 0 and N>20:
+    print('Not Weird')     

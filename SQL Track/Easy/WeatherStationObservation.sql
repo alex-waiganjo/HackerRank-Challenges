09cf18f8d4bda89d2ee557db181b030bc8bae161
alex-waiganjo <alexwaiganjo934@gmail.com>
2023-02-27 00:39:49 +0300

    station 2

--- SQL Track/Easy/WeatherStationObservation.sql
+++ SQL Track/Easy/WeatherStationObservation.sql
@@ -12,16 +12,36 @@
 Where LAT_N is the northern latitude and LONG_W is the western longitude.
 ' 
 
+
+
 '
-TASK 1
+    Weather Observation  Station 1
+
 Query a list of CITY and STATE from the STATION table.'
+
 'SOLUTION '
 SELECT CITY,STATE FROM STATION;
 
 
+
 '
-TASK 2
+     Weather Observation  Station 2
+
+Query the following two values from the STATION table:
+1. The sum of all values in LAT_N rounded to a scale of 2 Decimal Places
+2. The sum of all values in LONG_W rounded to a scale of 2 Decimal Places'
+'The Output must be in the form lat ,lon'
+
+'SOLUTION'
+SELECT  ROUND(SUM(LAT_N),2) AS lat  ,  ROUND(SUM(LONG_W),2) AS lon FROM STATION;
+
+
+
+'
+     Weather Observation  Station 3
+
 Query a list of CITY names from STATION for cities that have an even ID number.
 Print the results in any order, but exclude duplicates from the answer.'
+
 'SOLUTION'
 SELECT  DISTINCT CITY FROM STATION WHERE MOD(ID,2)=0;
